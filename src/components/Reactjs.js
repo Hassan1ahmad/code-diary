2102b58c14847f9d6c6a253f0ba676d2080d5664
Hassan Ahmad <hassan1ahmad999@gmail.com>
2023-11-09 23:00:10 +0500

    adding react-router-dom and redux toolkit

--- src/components/Reactjs.js
+++ src/components/Reactjs.js
@@ -11,7 +11,12 @@ function Reactjs() {
         <nav id="navbar-example3" className="sidebar">
           <a className="nav-link" href="#item-1">Create React App</a>
           <a className="nav-link" href="#item-2">Deploy on Git</a>
-          <a className="nav-link" href="#item-3">Item 3</a>
+          <a className="nav-link" href="#item-3">React Router dom</a>
+          <a className="nav-link" href="#item-4">Redux Toolkit</a>
+              <nav className="nav nav-pills flex-column">
+              <a className="nav-link ms-3 my-1" href="#item-4-1">Create Slice</a>
+              <a className="nav-link ms-3 my-1" href="#item-4-2">Store</a>
+            </nav>
         </nav>
       </div>
       <div className="col-8 content">
@@ -66,8 +71,143 @@ function Reactjs() {
 
           </div>
           <div id="item-3">
+            <h4>React Router dom</h4>
+            <p>
+             React Router is a crucial library for handling page routing in React applications, as React doesn't provide it out-of-the-box.
+          </p>
+
+          <p>
+            <b>Adding React Router dom:</b> To integrate React Router dom into your project, you can easily install it via your terminal using the following command:
+          </p>
+          <Code language='javascript' code={`npm i -D react-router-dom`} />
+          <p>
+            <b>Import it in app.js:</b> After installing React router dom, import it in the app.js
+          </p>
+          <Code language='javascript' code={`import { BrowserRouter, Routes, Route } from "react-router-dom";`} />
+          <p>
+             <b>Add routers:</b> Now ,add routes in the app.js 
+          </p>
+          <Code language='javascript' code={`<BrowserRouter>\n  <Routes>\n    <Route index element={<Home />} />\n    <Route path="/about" element={<About />} />\n    <Route path="/contact me" element={<Contact me />} />\n  </Routes>\n</BrowserRouter>`} />
+          <p>
+             <b>Add navigation:</b> Make all the Navigation links using <code>&lt;Link&gt;</code>  tag, like this:
+          </p>
+          <Code language='javascript' code={`<Link to="/">Home</Link> \n<Link to="/about">About</Link> \n<Link to="/contact me">Contact me</Link>`} />
+
+
+          </div>
+          <div id="item-4">
+            <h4>Redux Toolkit</h4>
+            <p>
+            <b>Step 1: Installing Redux Toolkit</b><br/>
+            To get started with Redux Toolkit, you need to install it using npm . Here's how you can install it using npm:
+          </p>
+          <Code language='javascript' code={`npm install @reduxjs/toolkit`} />
+
+
+          <p>
+            This command will download and install the Redux Toolkit package in your project.
+          </p>
+
+          <p>
+            <b>Step 2: Adding React Bindings</b><br/>
+            If you're working with React and want to connect your components to the Redux store, you'll need the react-redux library. Install it by running the following command:
+          </p>
+
+          <Code language='javascript' code={`npm install react-redux`} />
+
+
+          <p>
+            <code>react-redux</code> provides the necessary tools to connect your React components to the Redux store, allowing you to access and update the state in a more straightforward way.
+</p>
+
+          </div>
+          <div id="item-4-1">
+            <h4>Create slice</h4>
+                      <p>
+            Redux Toolkit is a powerful library for <strong>streamlining state management</strong> in your React applications. It simplifies the process of setting up your Redux store and managing state. Let's look at how you can <strong>create, update, and access your application's state</strong> using Redux Toolkit.
+          </p>
+
+          <p>
+            To get started with Redux Toolkit, you need to <strong>set up your Redux store</strong>. This is typically done in a separate file, such as store.js. You'll configure your store using configureStore from @reduxjs/toolkit, and you can include <strong>multiple slices</strong> to manage different parts of your application's state.
+          </p>
+
+          <p>
+            In each slice, you define the <strong>initial state</strong>, along with <strong>reducers</strong> and <strong>actions</strong> that specify how the state can be modified. Here's an example of a counter slice:
+          </p>
+
+          <Code language='javascript' code={`// counterSlice.js\nimport { createSlice } from '@reduxjs/toolkit';\n\nconst counterSlice = createSlice({\n  name: 'counter', // Name for the slice\n  initialState: 0,  // Initial state\n  reducers: {\n    increment: (state) => {\n      return state + 1;\n    },\n    decrement: (state) => {\n      return state - 1;\n    },\n  },\n});\n\nexport const { increment, decrement } = counterSlice.actions;\n\nexport default counterSlice.reducer;`} />
+
+
+          <p>
+            In this example, we create a slice named <strong>'counter'</strong> with an initial state of <strong>0</strong>. We define two reducers, <strong>'increment'</strong> and <strong>'decrement'</strong>, which modify the state by increasing or decreasing it by <strong>1</strong>. The  line exports the action creators for these actions.
+          </p>
+
+          <p>
+            Now, you can <strong>dispatch actions</strong> in your React components to interact with the state. For instance, you can dispatch the <strong>'increment'</strong> action when the user clicks an button to increment the counter.
+          </p>
+
+          <Code language='javascript' code={`// In your React component\nimport { useDispatch, useSelector } from 'react-redux';\nimport { increment, decrement } from './counterSlice'; // Import your counter slice\n\nfunction Counter() {\n  const dispatch = useDispatch();\n  const counter = useSelector(state => state.counter); // Select the 'counter' state\n\n  const handleIncrement = () => {\n    dispatch(increment());\n  }\n\n  const handleDecrement = () => {\n    dispatch(decrement());\n  }\n\n  return (\n    <div>\n      <p>Counter: {counter}</p>\n      <button onClick={handleIncrement}>Increment</button>\n      <button onClick={handleDecrement}>Decrement</button>\n    </div>\n  );\n}
+`} />
+
+
+          <p>
+            Redux Toolkit and slices make state management in your React application more efficient and maintainable. They <strong>reduce boilerplate code</strong> and <strong>encourage modularization</strong>, making it easier to develop and maintain your application's state.
+          </p>
+
+          </div>
+          <div id="item-4-2">
+            <h4>Store</h4>
+            <Code language='javascript' code={`// store.js\nimport { configureStore } from '@reduxjs/toolkit';\nimport counterReducer from './counterSlice'; // This is where the slice will be defined\n`} />
+            <p>
+            <b>Importing configureStore</b><br/>
+            In this part of the code, we initiate the process by importing the configureStore function from the @reduxjs/toolkit library. This function serves as a crucial tool for configuring and creating the Redux store.
+          </p>
+
+          <p>
+            <b>Importing counterReducer</b><br/>
+            Additionally, we import the counterReducer from the 'counterSlice' file. This import statement is used to associate the 'counter' part of the state with its corresponding reducer. However, it's essential to note that the actual implementation of this reducer is defined within the 'counterSlice' file. Think of this as a preparatory step to link the reducer to a specific section of the state.
+          </p>
+          <p>
+          Now, let's look at the configuration of the Redux store:
+          </p>
+
+          <Code language='javascript' code={`const store = configureStore({\n  reducer: {\n    counter: counterReducer, // We will define the counterSlice next\n  },\n});`} />
+          <p>
+          <b>Creating the Redux Store</b><br/>
+          We initiate the creation of the Redux store by invoking the configureStore function. Within this function, we provide an object with a reducer property.
+        </p>
+
+        <p>
+          <b>The Reducer Property</b><br/>
+          The reducer property within the configureStore function is an object that maps various segments of the application's state to their respective reducers. In this instance, we are defining a 'counter' part of the state and associating it with the counterReducer (which will be defined in the 'counterSlice' but is not yet defined at this point).
+        </p>
+
+        <p>
+          <b>Store Structure</b><br/>
+          This setup establishes the structure of the Redux store, precisely specifying which reducers are responsible for managing different sections of the state.
+        </p>
+
+        <p>
+        Finally, let's export the configured store:
+        </p>
+        <Code language='javascript' code={`export default store;`} />
+
+        <p>
+        <b>Exporting the Redux Store</b><br/>
+        We conclude this code by exporting the store object. This action makes the store available for use throughout other parts of your application. The store, in essence, will act as the central repository for managing your application's state.
+      </p>
+
+      <p>
+        <b>Summary</b><br/>
+        To summarize, this code establishes the Redux store using Redux Toolkit. It imports the required functions, preparing to connect the 'counter' section of the state to its corresponding reducer (which will be defined in the 'counterSlice' file). The configured store is then made accessible for use in the remainder of the application.
+      </p>
+
+
+
+          </div>
+          <div id="item-3">
             <h4>Item 3</h4>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto, deleniti repellendus ratione suscipit reprehenderit, natus voluptate libero, laboriosam porro voluptas nihil velit magni labore sed provident omnis aut itaque perferendis unde laborum et vitae! Dolore libero voluptatibus eveniet voluptate veniam praesentium iure aspernatur. Consectetur non sed dolore exercitationem suscipit rerum mollitia ratione dignissimos praesentium nihil perspiciatis quisquam corrupti fugit accusantium eius distinctio officia minima id, consequuntur alias incidunt, dolores laboriosam omnis. Vitae dolorem at nostrum consequatur, quidem sequi? Quisquam praesentium possimus quidem. Eum voluptas cum impedit expedita sequi, nihil, aperiam voluptate, quis quam ipsa tenetur repellendus itaque nesciunt. Autem sunt, suscipit est commodi, exercitationem, placeat deleniti repudiandae iusto et beatae vero temporibus! Alias odio ea corrupti porro quisquam hic, nam quibusdam, eum rerum veniam inventore nulla doloribus eius quae libero! Magnam quidem repudiandae quam laborum at consequuntur blanditiis porro dolor, nam, cum voluptatem facere laboriosam quae adipisci ullam. Iusto culpa libero reiciendis aliquam architecto perspiciatis quis vitae placeat voluptatibus praesentium, laborum maxime sequi sed minima asperiores dolores. Provident expedita iusto recusandae blanditiis vero, inventore ab consequuntur quasi ullam eaque doloribus iure officia obcaecati ea nostrum odio dolorum hic nihil. Consectetur, velit. Aspernatur atque dicta sed eum voluptatum eos obcaecati a nobis, veritatis deleniti consequuntur commodi iure, doloremque error veniam, non dolorum! Aliquam obcaecati sed, ut laudantium alias neque expedita, recusandae atque quia reprehenderit minima! Eos veniam, possimus ex numquam eveniet aut est maiores at porro perspiciatis accusantium voluptates eligendi autem quasi recusandae, consequuntur praesentium id. Sit architecto numquam cum maxime nostrum iusto maiores quaerat expedita voluptate neque quibusdam aliquam debitis amet dolor quas tempora eaque modi est tenetur, eveniet fugiat culpa? Autem nobis nihil ex numquam odit ipsam cumque quod, porro cum reiciendis quasi quam quia recusandae est aliquid nulla eveniet, laborum perferendis. Quaerat accusantium, beatae ducimus unde repudiandae laborum quo similique officia nulla eum quis amet saepe cumque fugiat? Facilis hic molestiae, repellat quia sit porro repellendus. Officia, libero ipsam reiciendis, ipsum nihil deserunt fuga expedita perspiciatis aperiam maxime nisi laborum similique illum ut quae? Veniam quasi magni quos aliquid hic voluptatum rem ipsum, quaerat dolore modi exercitationem et. Est ipsam culpa ratione facilis provident maxime ut saepe veniam magni. Cum, impedit molestiae optio necessitatibus, quasi magnam numquam aliquam, saepe iste soluta beatae aut provident vitae harum hic deleniti eius ut. Rerum illo odio necessitatibus magnam soluta excepturi, enim voluptate tempore maxime dolorem magni asperiores quasi eaque eum non quo recusandae, consectetur quisquam, neque unde! Minima expedita beatae culpa excepturi quo, non voluptas sapiente, pariatur repellendus et, illo nisi blanditiis asperiores ipsam. Corporis, pariatur! Voluptatum eos earum rem veritatis quod. Dolor reiciendis facere eaque perspiciatis nesciunt voluptate sint excepturi quia architecto quis doloribus odit, magni numquam dolore? Laborum, consectetur. Voluptatibus cumque ipsa possimus officia explicabo magnam eum dolores sit similique, magni recusandae nulla quis totam nam accusamus, corporis corrupti ea ab. Cumque impedit vero nesciunt distinctio obcaecati praesentium accusantium sapiente incidunt, voluptatum ad natus fugiat maxime culpa dolor repellat voluptates asperiores deserunt tenetur a deleniti iusto? Rerum, dolor voluptatibus.x</p>
+            <p></p>
           </div>
         </div>
       </div>
